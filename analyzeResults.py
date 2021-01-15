88a6c7a2ca21d570a457aadfcb0b85e92b6a529f
Stefan Körner <contact@skoerner.com>
2021-01-15 16:38:34 +0100

    Korrigierter Correlationsgraph

--- analyzeResults.py
+++ analyzeResults.py
@@ -1,70 +1,73 @@
-import pandas as pd
-import seaborn as sns
+import warnings
+
 import matplotlib.pyplot as plt
 import numpy as np
-import warnings
+import pandas as pd
+import seaborn as sns
 from scipy.stats import pearsonr
 
 if __name__ == '__main__':
-    df = pd.read_csv("./out/results.csv", sep=";", decimal=",").round(2)
+    df = pd.read_csv("./out/batch_results_2.0 - Rohdaten.csv", sep=",", decimal=".").round(2)
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     df = df.loc[:, ~df.columns.str.contains('SEED')]
-    corr = df.corr()
+    df = df.loc[:, ~df.columns.str.contains('ID')]
 
 
-    def calculate_pvalues(df):
-        df = df.dropna()._get_numeric_data()
-        dfcols = pd.DataFrame(columns=df.columns)
+    def calculate_pvalues(df_):
+        df_ = df_.dropna()
+
+        dfcols = pd.DataFrame(columns=df_.columns)
+        cvalues = dfcols.transpose().join(dfcols, how='outer')
         pvalues = dfcols.transpose().join(dfcols, how='outer')
 
-        # turn warnings off for the two loops
-        # pearsonnr returns a warning, when called with constants
         warnings.filterwarnings("ignore")
         for r in df.columns:
             for c in df.columns:
-                pvalues[r][c] = round(pearsonr(df[r], df[c])[1], 4)
+                c_, p_ = pearsonr(pd.to_numeric(df_[r], errors='coerce'), pd.to_numeric(df_[c], errors='coerce'))
+                pvalues[r][c] = round(p_, 4)
+                if np.isnan(p_) or np.isnan(c_) or p_ > 0.01:
+                    continue
+
+                c_ = round(c_, 4)
+                cvalues[r][c] = c_
+
         # turn warnings back on
         warnings.filterwarnings("always")
-        return pvalues
+
+        for col in cvalues:
+            cvalues[col] = pd.to_numeric(cvalues[col], errors='coerce')
+        for col in pvalues:
+            pvalues[col] = pd.to_numeric(pvalues[col], errors='coerce')
+
+        return cvalues, pvalues
 
 
-    p = calculate_pvalues(df.fillna(0)) # Alternative: df.replace(np.nan,0)
+    cols_to_fill = ['died_of_chance', 'children_per_woman', 'altruistic_acts_altruists', 'altruistic_acts_base_agents',
+                    'died_of_old_age', 'population_nonAltruists', 'population_altruists', 'population',
+                    'died_of_fitness_loss',
+                    'nonAltruist_fitness', 'net_growth']
 
-    mask_half = np.triu(np.ones_like(corr, dtype=bool)) # -> Values only once -> Dreieck
-    mask_sig = np.tril(p < 0.05) # Signifikanz
-    mask_combined =  mask_sig | mask_half # Beide Masken kombiniert
+    for col in cols_to_fill:
+        df[col] = df[col].fillna(0)
 
-    # Set up the matplotlib figure
+    c, p = calculate_pvalues(df)
+
+    mask_half = np.triu(np.ones_like(c, dtype=bool))  # -> Values only once -> Dreieck
+
     f, ax = plt.subplots(figsize=(11, 9))
 
-    # Generate a custom diverging colormap
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
 
-    # Draw the heatmap with the mask and correct aspect ratio
-    # It is important to have the order of graphs and savefig as it is (alternating)
-    # otherwise figure gets overwritten every time and results get mixed up
     sns.set_theme(style="white")
-    correlation_graph_half = sns.heatmap(corr, mask=mask_half, cmap=cmap, center=0,
-                square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    plt.figure()
-    plt.clf()
-    correlation_graph_half.figure.savefig("out/plot_figures/correlation_graph_half.png", bbox_inches='tight')
-
-    correlation_graph_half_sig = sns.heatmap(corr, mask=mask_sig, cmap=cmap, center=0,
+    correlation_graph_half = sns.heatmap(c, cmap=cmap, mask=mask_half, center=0,
                                          square=True, linewidths=.5, cbar_kws={"shrink": .5})
     plt.figure()
-    plt.clf()
-    correlation_graph_half_sig.figure.savefig("out/plot_figures/correlation_graph_half_sig.png", bbox_inches='tight')
+    plt.show()
 
-    correlation_graph_half_combined = sns.heatmap(corr, mask=mask_combined, cmap=cmap, center=0,
-                                         square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    plt.figure()
-    plt.clf()
-    correlation_graph_half_combined.figure.savefig("out/plot_figures/correlation_graph_half_combined.png", bbox_inches='tight')
+    correlation_graph_half.figure.savefig("out/plot_figures/correlation_graph.png", bbox_inches='tight')
 
     """
     correlation_graph_half.figure.show()
     correlation_graph_half_sig.figure.show()
     correlation_graph_half_combined.figure.show()
     """
-
